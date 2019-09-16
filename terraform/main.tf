2fdd2f05a195e739fc12c40c1dcf725e9f68e309
Roberth Strand <roberth@strandkonsult.no>
2019-09-17 01:28:00 +0200

    Rebased

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,46 +19,16 @@ module "aci" {
     memory_size           = "1"
     port_number           = "80"
 }
-# Azure Traffic Manager, profile
-resource "azurerm_traffic_manager_profile" "terraform" {
-    name                    = "trmg-${var.deploy-name}"
-    resource_group_name     = "${azurerm_resource_group.terraform.name}"
-    traffic_routing_method  = "Priority"
-    dns_config {
-        relative_name       = "${var.deploy-name}-dns"
-        ttl                 = 100
-    }
-    monitor_config {
-        protocol            = "http"
-        port                = 80
-        path                = "/"
-    }
-}
-# Azure Traffic Manager, endpoint in Azure
-resource "azurerm_traffic_manager_endpoint" "terraform" {
-    name                = "${var.deploy-name}-endpoint"
-    resource_group_name = "${azurerm_resource_group.terraform.name}"
-    profile_name        = "${azurerm_traffic_manager_profile.terraform.name}"
-    target              = "${module.aci.containergroup_fqdn}"
-    type                = "externalEndpoints"
-    weight              = 100
-}
 resource "azurerm_dns_cname_record" "terraform" {
     name                = "demo"
     zone_name           = "terraform.skyarkitektur.no"
     resource_group_name = "demo"
     ttl                 = 300
-    record              = "${azurerm_traffic_manager_profile.terraform.fqdn}"
+    record              = "${module.aci.containergroup_fqdn}"
 }
 output "fqdn" {
     value = "${module.aci.containergroup_fqdn}"
 }
 output "id" {
     value = "${module.aci.containergroup_id}"
-}
-output "endpoint-traffic_manager" {
-    value = "${azurerm_traffic_manager_profile.terraform.fqdn}"
-}
-output "dns-cname" {
-    value = "${azurerm_dns_cname_record.terraform.name}.${azurerm_dns_cname_record.terraform.record}"
 }
